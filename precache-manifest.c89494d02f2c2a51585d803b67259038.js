117c3b071bab8a2fa3498835a4c224407d66a818
namil9698 <69798566+namil9698@users.noreply.github.com>
2020-10-21 16:49:38 +0900

    Updates

---
+++ precache-manifest.c89494d02f2c2a51585d803b67259038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf7d982dc929bf34895b1c26ff2d3e6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1482cb8d3d3967c12e72",
+    "url": "/monsters-rolodex/static/css/main.3c7fd2f8.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1482cb8d3d3967c12e72",
+    "url": "/monsters-rolodex/static/js/main.046a15e2.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
